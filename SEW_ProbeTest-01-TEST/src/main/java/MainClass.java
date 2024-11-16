708b53ba15ed835dfd0327a5233c863a46e41819
JulianWarlischek <147165428+JulianWarlischek@users.noreply.github.com>
2024-11-16 22:28:14 +0100

    Add files via upload

---
+++ SEW_ProbeTest-01-TEST/src/main/java/MainClass.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class MainClass extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("MainClass", args);
+    }
+
+    public void settings() {fullScreen();}
+
+    public void setup() {
+        frameRate(1);
+    }
+
+    public void draw() {
+        //Add some Code here
+    }
+}
---
+++ SEW_ProbeTest-01-TEST/src/main/java/SortAndSearch.java
@@ -0,0 +1,5 @@
+public class SortAndSearch {
+    public static void main(String[] args) {
+        //Add some Code here
+    }
+}
